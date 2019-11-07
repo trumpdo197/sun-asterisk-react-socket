5e4ac41cb9af60a9c1b1c77d2733327711c722f9
Trung Đỗ <hou.dobaotrung@gmail.com>
2019-11-07 22:08:31 +0700

    Update

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-const socket = require('./socket');
+const io = require('./socket');
 
-socket.on('connection', () => {
+let ticketBuyingData = {};
+
+io.on('connection', socket => {
     socket.emit('welcome', 'Kết nối rồi babe');
+
+    socket.on('clientJoinTicketBuyingRoom', function(roomId) {
+        socket.join(roomId);
+
+        if (roomId in ticketBuyingData === false) {
+            ticketBuyingData[roomId] = {};
+        };
+
+        socket.emit('onJoinTicketBuyingRoom', ticketBuyingData[roomId]);
+
+        socket.on('clientSeatChoosing', payload => {
+            let { x, y, userId } = payload;
+c
+            userId = !userId ? null : userId;
+
+            if (typeof x !== 'number' || x < 0) return;
+            if (typeof y !== 'number' || y < 0) return;
+
+            if (x in ticketBuyingData[roomId] === false) {
+                ticketBuyingData[roomId][x] = {};
+            }
+
+            if (ticketBuyingData[roomId][x][y] === userId) {
+                delete ticketBuyingData[roomId][x][y];
+            }
+            else {
+                ticketBuyingData[roomId][x][y] = userId;
+            }
+
+            io.to(roomId).emit('onSeatChoosing', ticketBuyingData[roomId]);
+        })
+    });
 })
+
+setInterval(() => console.log(ticketBuyingData), 5000);
