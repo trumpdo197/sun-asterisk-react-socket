baf4d924f5eac29804ee3fcf41eea9062cc161e6
Trung Đỗ <hou.dobaotrung@gmail.com>
2019-11-12 22:09:31 +0700

    Remove unused flies.

--- api/rule/get.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const { getRulesDir } = require(`${appRoot}/bepos-utils`);
-
-module.exports = (req, res) => {
-    res.send(JSON.parse(fs.readFileSync(getRulesDir(req.query.fileId))));
-}
